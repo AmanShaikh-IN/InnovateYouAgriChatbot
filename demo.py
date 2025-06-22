a9fde30f48996178be69bcddf5e5ba1a79bc4b17
AmanShaikh <132216790+AmanShaikh-IN@users.noreply.github.com>
2025-06-23 01:26:58 +0530

    Update demo.py

--- demo.py
+++ demo.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from sentence_transformers import SentenceTransformer
 import numpy as np
 import faiss
@@ -8,15 +7,4 @@ import pandas as pd
 conn = sqlite3.connect("schemes.db")
 df = pd.read_sql_query("SELECT * FROM schemes", conn)
 
-=======
-from sentence_transformers import SentenceTransformer
-import numpy as np
-import faiss
-import sqlite3
-import pandas as pd
-
-conn = sqlite3.connect("schemes.db")
-df = pd.read_sql_query("SELECT * FROM schemes", conn)
-
->>>>>>> d194048 (Initial commit)
 print(df.shape)
